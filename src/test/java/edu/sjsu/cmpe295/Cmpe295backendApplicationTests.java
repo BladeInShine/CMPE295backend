471902ea5b80d914d7f1aa1e36d06c31d97b8707
Dechen Deng <dengdechen@gmail.com>
2016-02-21 21:30:28 -0800

    Code Base and initial commit

---
+++ src/test/java/edu/sjsu/cmpe295/Cmpe295backendApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.sjsu.cmpe295;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Cmpe295backendApplication.class)
+public class Cmpe295backendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
