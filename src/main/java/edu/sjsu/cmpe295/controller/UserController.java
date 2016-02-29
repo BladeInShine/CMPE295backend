81922f48e84747096c3ed6703e02d448ee17b982
angelqian <angelqian2@gmail.com>
2016-02-28 19:40:44 -0800

    add register/ login page; add test case in userController

--- src/main/java/edu/sjsu/cmpe295/controller/UserController.java
+++ src/main/java/edu/sjsu/cmpe295/controller/UserController.java
@@ -3,20 +3,13 @@ package edu.sjsu.cmpe295.controller;
 import edu.sjsu.cmpe295.domains.User;
 import edu.sjsu.cmpe295.repository.UserRepository;
 import edu.sjsu.cmpe295.service.UserService;
-import org.apache.coyote.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-import javax.validation.Valid;
 
 /**
  * Created by BladeInShine on 16/2/25.
@@ -37,23 +30,23 @@ public class UserController {
     @Inject
     private UserService userService;
 
-//    @Autowired
-//    public void UserController(UserService us){
-//        this.userService = us;
-//    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String test295() {
-        return "test";
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public String testGET(@PathVariable("id") String id, Object U) {
+        System.out.print(U.toString());
+        return "IN User Controller , hello world, get success";
     }
 
-    @RequestMapping(value = "/home")
-    public String home() {
-        return "hello world, home";
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public String testPOST(@RequestBody User U) {
+        System.out.print(U.toString());
+        return "IN User Controller , hello world, post";
     }
 
+
+    //  /user/register
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<?> register(@Valid @RequestBody User user) {
+    public ResponseEntity<?> register(/*@Valid */@RequestBody User user) {
         User queryUser = userRepository.findOneByEmail(user.getEmail());
         if (queryUser != null) return new ResponseEntity<>("Email address already in use", HttpStatus.BAD_REQUEST);
         userService.createUser(user);
---
+++ src/main/resources/static/register/register.controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function RegisterController(UserService, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            console.log("IN Register Controller : =============================" + vm.user.username);
+            UserService.GetByUsername(vm.user.username)
+                .then(function (response) {
+                    console.log("TEST " + response);
+                });
+
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        //$location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
